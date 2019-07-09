b7482612c4356b6ecb0b36ff9b3f09834f11dde2
M91406 <andreas.reiter@microchip.com>
2019-07-09 16:41:50 +0200

    Updated ADC configuration (not done yet)

--- dspic/src/init/init_adc.c
+++ dspic/src/init/init_adc.c
@@ -15,6 +15,9 @@
 
 volatile uint16_t init_adc_module(void) {
     
+    // Make sure power to peripheral is enabled
+    PMD1bits.ADC1MD = 0; // ADC Module Power Disable: ADC module power is enabled
+    
     // ADCON1L: ADC CONTROL REGISTER 1 LOW
     ADCON1Lbits.ADON = 0; // ADC Enable: ADC module is off during configuration
     
@@ -23,43 +26,64 @@ volatile uint16_t init_adc_module(void) {
     ADCON1Hbits.SHRRES = 0b11; // Shared ADC Core Resolution Selection: 12-bit resolution ADC resolution = 12-bit (0...4095 ticks)
     ADCON1Hbits.FORM = 0; // Fractional Data Output Format: Integer
 
-    ADCON2Lbits.SHRADCS = ADCON2_SHRADCS_DIV_MIN; // shared core clock divider = 2:1 (minimum)
-    ADCON2Lbits.SHRSAMC = ADCON2_SHRSAMC_0008; // 10x TAD sampling time 
-    ADCON2Lbits.REFCIE = ADCON2_REFCIE_OFF; // no error interrupt is triggered when Vref becomes available
-    ADCON2Lbits.REFERCIE = ADCON2_REFERCIE_OFF; // no error interrupt is triggered when Vref fails
-    ADCON2Lbits.REFERR = 0; // reset error flag
-    ADCON2Lbits.REFRDY = 0; // reset bandgap status bit
-    ADCON2Lbits.EIEN = ADCON2_EIEN_ON; // enable early interrupts
-    ADCON2Lbits.SHREISEL = ADCON2_SHREISEL_8TAD; // ADC interrupts are triggered 8 TAD clocks ahead of completion
+    ADCON2Lbits.REFCIE = 0;; // Band Gap and Reference Voltage Ready Common Interrupt Enable: Common interrupt is disabled for the band gap ready event
+    ADCON2Lbits.REFERCIE = 0; // Band Gap or Reference Voltage Error Common Interrupt Enable: Disabled
+    ADCON2Lbits.EIEN = 1; // Early Interrupts Enable: The early interrupt feature is enabled
+    ADCON2Lbits.PTGEN = 0; // External Conversion Request Interface: Disabled
+    ADCON2Lbits.SHREISEL = 0b111; // Shared Core Early Interrupt Time Selection: Early interrupt is set and interrupt is generated 8 TADCORE clocks prior to when the data are ready
+    ADCON2Lbits.SHRADCS = 0b0000001; // Shared ADC Core Input Clock Divider: 2:1 (minimum)
 
-    ADCON3Lbits.CLKSEL = ADCON3_CLKSEL_FOSC; //ADCON3_CLKSEL_AFVCODIV; // set ADC clock to auxiliary clock (common clock with PWM)
-    ADCON3Lbits.CLKDIV = ADCON3_CLKDIV_1; // set ADC input clock divider
-    ADCON3Lbits.SHREN = ADCON3_CxEN_ENABLED; // enable shared ADC core
-    ADCON3Lbits.CNVCHSEL = ADCON3_CNVCHSEL_AN0; // set AN0 to be target for software triggers (not used)
-    ADCON3Lbits.CNVRTCH = ADCON3_CNVRTCH_READY; // reset TRIGGER READY flag bit
-    ADCON3Lbits.REFSEL = ADCON3_REFSEL_AVDD_AVSS; // set ADC reference to AVDD-toAVSS
-    ADCON3Lbits.SHRSAMP = ADCON3_SHRSAMP_HWTRIG; // use hardware trigger
-    ADCON3Lbits.SUSPCIE = ADCON3_SUSPCIE_DISABLED; // disable ISR SUSPEND interrupt 
-    ADCON3Lbits.SUSPEND = ADCON3_SUSPEND_RUN; // disable SUSPEND MODE
-    ADCON3Lbits.SUSPRDY = ADCON3_SUSPRDY_RUNNING; // reset SUSPEND mode status flag bit
-    ADCON3Lbits.SWCTRG = ADCON3_SWCTRG_READY; // set software trigger to READY
-    ADCON3Lbits.SWLCTRG = ADCON3_SWLCTRG_LVLTRG_BY_HW; // set level sensitive triggers for hardware
-    ADCON3Lbits.CNVCHSEL = ADCON3_CNVCHSEL_AN0; // configure the ADC inputs used (not used))
+    ADCON2Hbits.SHRSAMC = 8; // Shared ADC Core Sample Time Selection: 8x TADs sampling time 
+    ADCON2Hbits.REFERR = 0; // reset error flag
+    ADCON2Hbits.REFRDY = 0; // reset bandgap status bit
+
+    ADCON3Lbits.REFSEL = 0b000; // ADC Reference Voltage Selection: AVDD-toAVSS
+    ADCON3Lbits.SUSPEND = 0; // All ADC Core Triggers Disable: All ADC cores can be triggered
+    ADCON3Lbits.SUSPCIE = 0; // Suspend All ADC Cores Common Interrupt Enable: Common interrupt is not generated for suspend ADC cores
+    ADCON3Lbits.SUSPRDY = 0; // All ADC Cores Suspended Flag: ADC cores have previous conversions in progress
+    ADCON3Lbits.SHRSAMP = 0; // Shared ADC Core Sampling Direct Control: use hardware trigger
+    ADCON3Lbits.CNVRTCH = 0; // Software Individual Channel Conversion Trigger: Next individual channel conversion trigger can be generated (not used)
+    ADCON3Lbits.SWLCTRG = 0; // Software Level-Sensitive Common Trigger: No software, level-sensitive common triggers are generated (not used)
+    ADCON3Lbits.SWCTRG = 0; // Software Common Trigger: Ready to generate the next software common trigger (not used)
+    ADCON3Lbits.CNVCHSEL = 0; // Channel Number Selection for Software Individual Channel Conversion Trigger: AN0 (not used)
     
-    ADCON4Hbits.C0CHS = ADCON4_C0CHS_S1AN0;
-    ADCON4Hbits.C1CHS = ADCON4_C1CHS_S1AN1;
-    ADCON4Lbits.SAMC0EN = ADCON4_SAMCxEN_DISABLED;
-    ADCON4Lbits.SAMC1EN = ADCON4_SAMCxEN_DISABLED;
+    
+    ADCON3Hbits.CLKSEL = 0b01; // ADC Module Clock Source Selection: AVCODIV
+    ADCON3Hbits.CLKDIV = 0b000000; // ADC Module Clock Source Divider: 1 Source Clock Period
+    ADCON3Hbits.SHREN = 0; // Shared ADC Core Enable: Shared ADC core is disabled
+    ADCON3Hbits.C0EN = 0; // Dedicated ADC Core 0 Enable: Dedicated ADC Core 0 is disabled
+    ADCON3Hbits.C1EN = 0; // Dedicated ADC Core 1 Enable: Dedicated ADC Core 1 is disabled
+    
+    ADCON4Lbits.SAMC0EN = 0;  // Dedicated ADC Core 0 Conversion Delay Enable: Immediate conversion
+    ADCON4Lbits.SAMC1EN = 0;  // Dedicated ADC Core 1 Conversion Delay Enable: Immediate conversion
+    
+    ADCORE0Lbits.SAMC = 0b0000000000;   // Dedicated ADC Core 0 Conversion Delay Selection: 2 TADCORE (minimum)
+    ADCORE1Lbits.SAMC = 0b0000000000;   // Dedicated ADC Core 1 Conversion Delay Selection: 2 TADCORE (minimum)
 
-    ADCORE1Lbits.SAMC = 10;
+    // ADCON4H: ADC CONTROL REGISTER 4 HIGH
+    ADCON4Hbits.C0CHS = 0b00; // Dedicated ADC Core 0 Input Channel Selection: AN0
+    ADCON4Hbits.C1CHS = 0b01; // Dedicated ADC Core 1 Input Channel Selection: ANA1
+
+    // ADCON5L: ADC CONTROL REGISTER 5 LOW
+    // ADCON5Lbits.SHRRDY: Shared ADC Core Ready Flag (read only)
+    // ADCON5Lbits.C0RDY: Dedicated ADC Core 0 Ready Flag (read only)
+    // ADCON5Lbits.C1RDY: Dedicated ADC Core 1 Ready Flag (read only)
+    ADCON5Lbits.SHRPWR = 0; // Shared ADC Core Power Enable: ADC core is off
+    ADCON5Lbits.C0PWR = 0; // Dedicated ADC Core 0 Power Enable: ADC core is off
+    ADCON5Lbits.C1PWR = 0; // Dedicated ADC Core 1 Power Enable: ADC core is off
+    
+/*    
+    
+    ADCON5Lbits.SHRPWR = ADCON5_CxPWR_OFF; // Power off Shared Core => will be turned on later
+
+    ADCON5Hbits.SHRCIE = ADCON5L_CxCIE_DISABLED; // disable shared core to generate common interrupts
+    ADCON5Hbits.WARMTIME = ADCON5H_WARMTIME_CLK_32768; // Set warm-up time of ADC module
+    
     ADCORE1Hbits.RES = ADCON1_SHRRES_12BIT;
     ADCORE1Hbits.ADCS = ADCON2_SHRADCS_DIV_MIN; // shared core clock divider = 2:1 (minimum)
     ADCORE1Hbits.EISEL = ADCON2_SHREISEL_8TAD;
     
-    ADCON5Hbits.SHRCIE = ADCON5L_CxCIE_DISABLED; // disable shared core to generate common interrupts
-    ADCON5Lbits.SHRPWR = ADCON5_CxPWR_OFF; // Power off Shared Core => will be turned on later
-    ADCON5Hbits.WARMTIME = ADCON5H_WARMTIME_CLK_32768; // Set warm-up time of ADC module
-    
+  */  
     
     return(1);
 }
